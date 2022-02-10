0d87fbbf6b1431a25f31d1356c4366e5df3a7888
joshua Tkachuk <joshuatkachuk530@gmail.com>
2022-02-10 12:45:16 -0800

    first_react_app

---
+++ REACT/firstReactApp/my-first-project/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Hello Dojo!</h1>
+      </header>
+        <h2>Things I need to Do:</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
