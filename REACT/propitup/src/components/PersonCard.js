49647d329ad5d55d00ff79bf471c7f7d84e8fac6
joshua Tkachuk <joshuatkachuk530@gmail.com>
2022-02-15 13:51:34 -0800

    using state

--- REACT/propitup/src/components/PersonCard.js
+++ REACT/propitup/src/components/PersonCard.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 const PersonCard = (props) =>{
+    const [bDay, setBDay] = useState(props.age);
     return(
         <div>
             <h1>{props.lastname}, {props.firstname}</h1>
-            <p>Age: {props.age}</p>
+            <p>Age: {bDay}</p>
             <p>Hair Color: {props.hairColor}</p>
+            <button onClick={(event) =>setBDay(bDay + 1)}>Birthday Button for {props.firstname + " " + props.lastname}</button>
         </div>
     );
 }
